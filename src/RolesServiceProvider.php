64f8b71385c7c3e8aff9f7d8d9ccb3c24267872d
Edward Kawas <ekawas@polyglot42.com>
2019-09-24 09:47:48 -0700

    nitpick-CI fixes

--- src/RolesServiceProvider.php
+++ src/RolesServiceProvider.php
@@ -63,6 +63,7 @@ class RolesServiceProvider extends ServiceProvider
             $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
         }
     }
+
     /**
      * Loads a seeds.
      *
