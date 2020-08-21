a499bceedc4c8a5bff06f600c526bce1a626c6c7
hgasoft <49619916+hgasoft@users.noreply.github.com>
2020-08-21 14:27:20 +0300

    Update StorePermissionRequest.php

--- src/App/Http/Requests/StorePermissionRequest.php
+++ src/App/Http/Requests/StorePermissionRequest.php
@@ -31,7 +31,7 @@ class StorePermissionRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'          => 'required|max:60|unique:'.config('roles.permissionsTable').',name,'.$this->id,
+            'name'          => 'required|max:60|unique:'.config('roles.permissionsTable').',name'.($this->id ? ','.$this->id . ',id' : ''),
             'slug'          => 'required|max:60|unique:'.config('roles.permissionsTable').',slug,'.$this->id,
             'description'   => 'nullable|string|max:255',
             'model'         => 'required|string|max:60',
